c55910bb901d4067e497c514cbe5ab1c35c774f9
m-kodama <tc003ssr@gmail.com>
2017-10-27 11:42:51 +0900

    インデントを調整できるように変更

--- public_html/kadai3/hello-world.php
+++ public_html/kadai3/hello-world.php
@@ -4,15 +4,12 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<title>第3回 課題1</title>
-
 	</head>
 
 	<body>
-
 		<?php
 			echo("Hello, world!");
 		?>
-
 	</body>
 </html>
 
