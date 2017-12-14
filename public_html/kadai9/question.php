f5eab2d754244cad53ca55177d2f3e7cbc444f24
Masaaki Kodama <tc003ssr@gmail.com>
2017-12-14 13:18:59 +0900

    ランダムで問題を出題する際に問題データを全件取得するように変更
    ei_questionsテーブルのnumberデータは歯抜けの可能性があるため、問題データを全件取得した後にその中からランダムで1つ取得するように変更

--- public_html/kadai9/question.php
+++ public_html/kadai9/question.php
@@ -8,29 +8,23 @@
 		<h1>問題</h1>
 		<form action="answer.php" method="POST">
 			<?php
-			// 問題データ総数の取得
 			$dbname="b7fm1007";
 			$c = pg_connect("dbname=$dbname");
 			if($c == false) exit(0);
 			try {
-				// numberの最大値を取得
-				$query = "select MAX(number) from ei_questions;";
+				// 問題データ取得
+				$query = "select number, course, question, unit from ei_questions;";
 				if(!($r = pg_query($c, $query))) throw new Exception("Query failed(1).");
 				$m = pg_num_rows($r);
 				if($m < 0) throw new Exception("問題が登録されていません。");;
-				$row = pg_fetch_assoc($r, 0);
-				$number = $row["max"];
-				// 出題する問題のnumberを乱数により決定しDBから取得
-				$questionNumber = mt_rand(1, $number);
-				$query = "select course, question, unit from ei_questions ".
-									"where number = ".$questionNumber.";";
-				if(!($r = pg_query($c, $query))) throw new Exception("Query failed(2).");
-				$row = pg_fetch_assoc($r, 0);
+				// 出題する問題をランダムで一つ取得
+				$randomIndex = mt_rand(1, $m);
+				$row = pg_fetch_assoc($r, $randomIndex);
 				// 出題
 				echo "<p>[".$row["course"]."]<br>";
 				echo $row["question"]."<br><br>";
 				echo "答え：<input type=\"text\" name=\"answer\" size=\"50\">$unit";
-				echo "<input type=\"hidden\" name=\"number\" value=\"$questionNumber\"></p>";
+				echo "<input type=\"hidden\" name=\"number\" value=\"".$row["number"]."\"></p>";
 			} catch(Exception $e) {
 				echo "<span style='color:#D32F2F;'>".$e->getMessage()."</span>";
 			}
